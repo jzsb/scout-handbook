d79b9bfadf0446ca3f207d82f523de5383f7ffe6
jzsb <jzsbyt@gmail.com>
2021-06-01 22:13:54 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4bf45b4bdd68163e0e317a5403e0923'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '50e01e03-9c87-40bd-9fe0-ab8bb458097f'
+};
